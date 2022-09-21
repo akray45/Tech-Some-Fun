32239076517769319b6550adbb42e092960022d6
akray45 <akprpa@gmail.com>
2022-09-21 14:05:18 +0530

    first commit

---
+++ base/api/urls.py
@@ -0,0 +1,9 @@
+from xml.etree.ElementInclude import include
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.getRoutes),
+    path('rooms/', views.getRooms),
+    path('room/<str:pk>/', views.getRoom),
+]
